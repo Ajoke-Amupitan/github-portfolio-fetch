c5315c36bedcceb94ec73cab04eba79c632f2137
*Ajoke <*amupitanajoke2019@gmail.com*>
2022-11-10 17:56:19 +0100

    projects component defined

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Profile from './Pages/Profile';
-import logo from './assets/logo.svg';
+import Projects from './Pages/Projects';
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-      <Header logo={logo} />
-      {<Profile  userName='Ajoke-Amupitan'  />}
+      <BrowserRouter>
+
+      <Header />
+      {/* <Profile  userName=''  /> */}
+      <Routes>
+           <Route 
+             path='*'
+             element={<Profile userName='Ajoke-Amupitan' />}
+           />
+           <Route path='/projects' element=
+                 {<Projects userName='Ajoke-Amupitan' />} />
+         </Routes>
+    
+      </BrowserRouter>
           </div>
   );
 }
